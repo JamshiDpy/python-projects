e12d00aa40845ec80826096b7458ab0688f196b8
JamshiDpy <91729987+JamshiDpy@users.noreply.github.com>
2023-09-29 23:28:58 +0500

    Update qrcodegenerator.py

--- qrcodegenerator/qrcodegenerator.py
+++ qrcodegenerator/qrcodegenerator.py
@@ -1,2 +1,18 @@
-pip install qrcode
-pip instal image
+# pip install qrcode
+# pip instal image
+
+import qrcode
+import image
+
+qr_code = qrcode.QRCode(
+    version=15,
+    box_size=5,
+    border=2
+)
+
+data = "google.com"
+
+qr_code.add_data(data)
+qr_code.make(fit=True)
+img = qr_code.make_image(fill='black', back_color='white')
+img.save('google_qrcode.jpg')
